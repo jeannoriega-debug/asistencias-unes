23026a9ab8ba6ca81a6fa7a4ec3247f26bfc87ba
jeannoriega-debug <jeannoriega@gmail.com>
2026-05-24 12:20:28 -0400

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,6 +2,8 @@
 const SUPABASE_URL = 'https://sweoveheeayloqvtgjzu.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN3ZW92ZWhlZWF5bG9xdnRnanp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwODE5NDcsImV4cCI6MjA5MTY1Nzk0N30.y67qfbERRgnMDAxzTSPXJMHQaWWIX96Vha1KoDZZ8IE';
 
+window.SUPABASE_URL = SUPABASE_URL;
+window.SUPABASE_KEY = SUPABASE_KEY;
 // ✅ Crear el cliente de Supabase
 if (typeof supabase !== 'undefined') {
     window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
